6609815771b1990e0289af7863f7644e6abe6ff4
Greg Engle <Greg Engle>
2017-03-01 22:20:03 -0600

    Add project files.

---
+++ ConsoleApp1/Actors/WorkPartnerActor.cs
@@ -0,0 +1,114 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Actors
+{
+    public class SqlDataInfo
+    {
+        public string Server { get; }
+        public string Database { get; }
+        public SqlDataInfo(string server, string database)
+        {
+            this.Server = server;
+            this.Database = database;
+        }
+    }
+
+    public class CwsAbbreviation
+    {
+        public string Name { get; }
+        public CwsAbbreviation(string name)
+        {
+            this.Name = name;
+        }
+    }
+    public class UsageState
+    {
+        public bool Disabled { get; }
+        public UsageState(bool disabled)
+        {
+            this.Disabled = disabled;
+        }
+    }
+    public class WorkPartnerActor: ReceiveActor
+    {
+        private SqlDataInfo sqlInfo;
+        private CwsAbbreviation cwsAbbreviation;
+        private bool Disabled { get; set; }
+
+
+        public WorkPartnerActor(string abbreviation, bool disabled)
+        {
+            cwsAbbreviation = new CwsAbbreviation(abbreviation);
+            this.Disabled = disabled;
+            this.Receive<SqlDataInfo>(x =>
+            {
+                sqlInfo = x;
+            });
+            this.Receive<CwsAbbreviation>(x =>
+            {
+                cwsAbbreviation = x;
+            });
+            this.Receive<UsageState>(x =>
+            {
+                Disabled = x.Disabled;
+            });
+            this.Receive<LogStatus>(x =>
+            {
+                Console.WriteLine($"{cwsAbbreviation.Name} Disabled:{Disabled}");
+            });
+        }
+    }
+    public class LogStatus
+    {
+
+    }
+    public class WorkCapabilityActor: TypedActor
+    {
+        public WorkCapabilityActor()
+        {
+
+        }
+    }
+
+    public class NewWorkPartner
+    {
+        public string Name { get; }
+        public NewWorkPartner(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    public class MetaCoordinatorActor: ReceiveActor
+    {
+        HashSet<string> _workPartners = new HashSet<string>();
+
+        public MetaCoordinatorActor()
+        {
+            Receive<NewWorkPartner>(x =>
+            {
+                if (!_workPartners.Any(y=>y == x.Name))
+                {
+                    _workPartners.Add(x.Name);
+                    var props = Props.Create(() => new WorkPartnerActor(x.Name ,true));
+                    Context.ActorOf(props, x.Name);
+                }
+            });
+            Receive<LogStatus>(x =>
+            {
+                var actor = Context.ActorSelection("/user/metaCoordinator/*");
+                actor.Tell(x);
+
+                //foreach (var wp in _workPartners)
+                //{
+                //    Console.WriteLine($"Partner: {wp}");
+                //}
+            });
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using ConsoleApp1.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var akkaConfig = @"
+akka {  
+    stdout-loglevel = DEBUG
+    loglevel = DEBUG
+    log-config-on-start = on        
+    actor {                
+        debug {  
+              receive = on 
+              autoreceive = on
+              lifecycle = on
+              event-stream = on
+              unhandled = on
+        }
+    }";
+            var config = ConfigurationFactory.ParseString(akkaConfig);
+            // Create a new actor system (a container for your actors)
+            var system = ActorSystem.Create("schedulerSystem", config);
+            var coordinatorProps = Props.Create<MetaCoordinatorActor>();
+            var coordinator = 
+                system.ActorOf(coordinatorProps, "metaCoordinator");
+           
+            coordinator.Tell(new NewWorkPartner("CCI"));
+            coordinator.Tell(new NewWorkPartner("AMI"));
+
+            Thread.Sleep(1000);
+            //coordinator.Tell(new LogStatus());
+            
+            var actor= system.ActorSelection("/user/metaCoordinator/CCI");
+            actor.Tell(new UsageState(disabled: false));
+
+            coordinator.Tell(new LogStatus());
+            system.WhenTerminated.Wait();
+            // This prevents the app from exiting
+            // before the async work is done
+            Console.ReadLine();
+        }
+    }
+}
